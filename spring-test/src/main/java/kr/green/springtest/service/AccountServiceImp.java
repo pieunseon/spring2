56f94fcbaee6220386f5b735945432bfc3d83e08
pieunseon <kozizi79@naver.com>
2019-01-23 11:11:06 +0900

    AccountVo,Dao,Service,Imp 생성

---
+++ spring-test/src/main/java/kr/green/springtest/service/AccountServiceImp.java
@@ -0,0 +1,8 @@
+package kr.green.springtest.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImp implements AccountService{
+
+}
---
+++ spring-test/src/main/java/kr/green/springtest/vo/AccountVo.java
@@ -0,0 +1,11 @@
+package kr.green.springtest.vo;
+
+import java.util.Date;
+
+public class AccountVo {
+	private String id;
+	private String pw;
+	private String gender;
+	private Date registered_date;
+	private String authority;
+}
