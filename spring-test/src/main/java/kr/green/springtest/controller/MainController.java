9807546e79e38950b9cbec4a4aec5670aaf90064
pieunseon <kozizi79@naver.com>
2019-01-23 10:07:58 +0900

    로그인 정보 콘솔 출력 테스트

--- spring-test/src/main/java/kr/green/springtest/controller/MainController.java
+++ spring-test/src/main/java/kr/green/springtest/controller/MainController.java
@@ -1,4 +1,4 @@
-package kr.green.springtest;
+package kr.green.springtest.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,29 +11,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
+public class MainController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String homeGet(Locale locale, Model model) {
 		return "home";
 	}
-	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String homePost(Model model,String id, String pw) {
+		System.out.println("id : "+id+", pw: " +pw);
+		return "redirect:/";
+	}
 }
