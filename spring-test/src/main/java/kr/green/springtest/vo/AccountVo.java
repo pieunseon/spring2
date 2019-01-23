ca677a328ddbfcfac54a824d1b1350f1c9b1bfc4
pieunseon <kozizi79@naver.com>
2019-01-23 12:01:23 +0900

    AccountMapper

--- spring-test/src/main/java/kr/green/springtest/vo/AccountVo.java
+++ spring-test/src/main/java/kr/green/springtest/vo/AccountVo.java
@@ -8,4 +8,40 @@ public class AccountVo {
 	private String gender;
 	private Date registered_date;
 	private String authority;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getRegistered_date() {
+		return registered_date;
+	}
+	public void setRegistered_date(Date registered_date) {
+		this.registered_date = registered_date;
+	}
+	public String getAuthority() {
+		return authority;
+	}
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+	@Override
+	public String toString() {
+		return "AccountVo [id=" + id + ", pw=" + pw + ", gender=" + gender + ", registered_date=" + registered_date
+				+ ", authority=" + authority + "]";
+	}
+	
 }
