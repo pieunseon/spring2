291522ea00f03743e797792f91a41194474fda15
pieunseon <kozizi79@naver.com>
2019-01-23 12:26:18 +0900

    에러 수정-로그인
    비회원 아이디로 로그인 시도시 발생하는 에러수정

--- spring-test/src/main/java/kr/green/springtest/service/AccountServiceImp.java
+++ spring-test/src/main/java/kr/green/springtest/service/AccountServiceImp.java
@@ -18,7 +18,7 @@ public class AccountServiceImp implements AccountService{
 	@Override
 	public AccountVo signin(AccountVo loginInfo) {
 		AccountVo user = accountDao.getAccount(loginInfo.getId());  //암호화된 정보이기 때문에.먼저 Id 정보를 넣어서 객체정보를 확인 후에 암호화 작업 처리 위해서 
-		if(passwordEncoder.matches(loginInfo.getPw(),user.getPw()))
+		if(user != null && passwordEncoder.matches(loginInfo.getPw(),user.getPw()))
 			return user;
 		return null;
 	} 
