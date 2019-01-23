f543766c8bd7fe75810ce0c3d76ec8118b21843a
pieunseon <kozizi79@naver.com>
2019-01-23 12:00:54 +0900

    MainController

--- spring-test/src/main/java/kr/green/springtest/controller/MainController.java
+++ spring-test/src/main/java/kr/green/springtest/controller/MainController.java
@@ -6,22 +6,28 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.green.springtest.service.AccountService;
+import kr.green.springtest.vo.AccountVo;
+
 @Controller
 public class MainController {
-	
+	@Autowired
+	AccountService accountService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet(Locale locale, Model model) {
 		return "home";
 	}
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String homePost(Model model,String id, String pw) {
-		System.out.println("id : "+id+", pw: " +pw);
+	public String homePost(Model model, AccountVo loginInfo) {
+		 AccountVo user =accountService.signin(loginInfo);
+		 System.out.println(user);
 		return "redirect:/";
 	}
 }
