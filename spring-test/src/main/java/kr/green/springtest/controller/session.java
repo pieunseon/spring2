0e3ae223473925485e07632d7b1fdb9ec34e1b2b
pieunseon <kozizi79@naver.com>
2019-01-25 10:45:25 +0900

    session

---
+++ spring-test/src/main/java/kr/green/springtest/controller/session.java
@@ -0,0 +1,12 @@
+package kr.green.springtest.controller;
+
+import kr.green.springtest.vo.AccountVo;
+
+public class session {
+
+	public static AccountVo getAttribute(String string) { 
+		 
+		return null;
+	}
+
+}
